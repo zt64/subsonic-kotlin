076f6f818984e56fe08519e0c8575f16e0c2058f
zt64 <31907977+zt64@users.noreply.github.com>
2026-03-09 00:39:37 -0400

    fix: add custom serializer for time in ChatMessage

--- subsonic-api/src/commonMain/kotlin/dev/zt64/subsonic/api/model/ChatMessage.kt
+++ subsonic-api/src/commonMain/kotlin/dev/zt64/subsonic/api/model/ChatMessage.kt
@@ -1,7 +1,25 @@
 package dev.zt64.subsonic.api.model
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlin.time.Instant
+
+internal object EpochSerializer : KSerializer<Instant> {
+    override val descriptor =
+        PrimitiveSerialDescriptor("kotlinx.datetime.Instant", PrimitiveKind.LONG)
+
+    override fun deserialize(decoder: Decoder): Instant {
+        return Instant.fromEpochMilliseconds(decoder.decodeLong())
+    }
+
+    override fun serialize(encoder: Encoder, value: Instant) {
+        encoder.encodeLong(value.toEpochMilliseconds())
+    }
+}
 
 /**
  * A chat message
@@ -13,6 +31,7 @@ import kotlin.time.Instant
 @Serializable
 public data class ChatMessage internal constructor(
     val username: String,
+    @Serializable(EpochSerializer::class)
     val time: Instant,
     val message: String
 )
