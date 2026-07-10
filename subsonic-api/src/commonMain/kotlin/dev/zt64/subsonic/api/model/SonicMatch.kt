46757ac657606fd5f48270f83b137f392b52e43c
zt64 <31907977+zt64@users.noreply.github.com>
2026-07-10 16:53:29 -0400

    fix: make SonicMatch serializable

--- subsonic-api/src/commonMain/kotlin/dev/zt64/subsonic/api/model/SonicMatch.kt
+++ subsonic-api/src/commonMain/kotlin/dev/zt64/subsonic/api/model/SonicMatch.kt
@@ -1,6 +1,7 @@
 package dev.zt64.subsonic.api.model
 
 import dev.zt64.subsonic.api.SubsonicApi
+import kotlinx.serialization.Serializable
 
 /**
  * Sonic similarity match
@@ -10,7 +11,8 @@ import dev.zt64.subsonic.api.SubsonicApi
  * For getSonicSimilarTracks, relative to the query song. For [SubsonicApi.findSonicPath], \
  * relative to the starting song. Returns `-1` when similarity is not supported by the server.
  */
-public data class SonicMatch private constructor(
+@Serializable
+public data class SonicMatch internal constructor(
     val entry: SubsonicResource,
     val similarity: Float
 )
